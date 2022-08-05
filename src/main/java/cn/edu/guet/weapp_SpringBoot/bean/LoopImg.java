874ab53e96b9b5b87210832f2a537cef35a50e66
Toooyounger <1977751861@qq.com>
2022-08-05 20:26:13 +0800

    初步提交轮播图模块

---
+++ src/main/java/cn/edu/guet/weapp_SpringBoot/bean/LoopImg.java
@@ -0,0 +1,40 @@
+package cn.edu.guet.weapp_SpringBoot.bean;
+
+public class LoopImg {
+    private String id;
+    private String url;
+    private String subtitle;
+    private String title;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getSubtitle() {
+        return subtitle;
+    }
+
+    public void setSubtitle(String subtitle) {
+        this.subtitle = subtitle;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
---
+++ src/main/java/cn/edu/guet/weapp_SpringBoot/controller/CarouselController.java
@@ -0,0 +1,75 @@
+package cn.edu.guet.weapp_SpringBoot.controller;
+
+
+
+import cn.edu.guet.weapp_SpringBoot.bean.LoopImg;
+import cn.edu.guet.weapp_SpringBoot.http.HttpResult;
+import cn.edu.guet.weapp_SpringBoot.mapper.SysCarouselMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+
+@RestController
+public class CarouselController {
+    private String filePath  = "/root/img/";
+    private String fileName = "";
+    private String baseUrl="http://47.95.223.172/img/";
+    private String src = "";
+    @Autowired
+    private SysCarouselMapper sysCarouselMapper;
+
+    @PostMapping(value = "/uploadPic")
+    public HttpResult getPic(MultipartFile file) throws IOException {
+        System.out.println(file);
+        System.out.println("开始文件上传");
+        String realName = file.getOriginalFilename();
+        System.out.println("文件名："+realName);
+        File file1 = new File(filePath);
+        if (!file1.exists()){
+            file1.mkdirs();
+        }
+        fileName = filePath+ realName;
+        src = baseUrl+realName;
+        file.transferTo(new File(fileName));
+        
+        return HttpResult.ok();
+    }
+
+    @PostMapping(value = "/updataPic")
+    public HttpResult updataPic(@RequestBody LoopImg loopImg){
+        loopImg.setUrl(src);
+        sysCarouselMapper.updataPic(loopImg);
+        return HttpResult.ok();
+    }
+
+
+    @PostMapping(value = "/newPic")
+    public HttpResult addNewPic(@RequestBody LoopImg loopImg){
+        loopImg.setUrl(src);
+        sysCarouselMapper.insertPic(loopImg);
+        return HttpResult.ok(loopImg);
+    }
+
+
+    @PostMapping(value = "/delPic")
+    public HttpResult deletePic(@RequestBody LoopImg loopImg){
+        System.out.println(loopImg.getId());
+        sysCarouselMapper.delPic(loopImg.getId());
+        return HttpResult.ok();
+    }
+
+    @GetMapping(value = "/getCarousel")
+    public HttpResult getCarousel(){
+        System.out.println("获取轮播图");
+        List<LoopImg> myImg = sysCarouselMapper.getCarousel();
+        return HttpResult.ok(myImg);
+    }
+}
